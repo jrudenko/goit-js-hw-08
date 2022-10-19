9aa11d711d85a84e7cd36c1b172ce1518ba697db
Julia Rudenko <yrudenko@i.ua>
2022-10-19 13:59:26 +0300

    converting

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,11 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
+
+
+
 
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryItemMarkup(galleryItems);
