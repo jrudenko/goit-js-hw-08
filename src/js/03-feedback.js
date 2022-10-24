8b936113c66f2c03f6a1565e9ad934959aa54c6a
Julia Rudenko <yrudenko@i.ua>
2022-10-24 18:07:25 +0300

    Пофіксила помилки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,9 @@ import '../css/common.css';
 import '../css/03-feedback.css';
 
 import throttle from 'lodash.throttle';
-
 const STORAGE_KEY = 'feedback-form-state';
-const getData = localStorage.getItem(STORAGE_KEY);
-let formData = {};
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -16,24 +15,26 @@ const refs = {
 refs.form.addEventListener('submit', onFormSummit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
+populateTextarea();
+
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
-
-populateTextarea();
 
 function onFormSummit(e) {
   e.preventDefault();
   console.log('Отправка');
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  formData = {};
 }
 
 function populateTextarea() {
+  const getData = localStorage.getItem(STORAGE_KEY);
   const savedMassage = JSON.parse(getData);
   if (savedMassage) {
-    refs.input.value = savedMassage.input;
-    refs.textarea.value = savedMassage.textarea;
+    refs.input.value = savedMassage.mail || '';
+    refs.textarea.value = savedMassage.message || '';
   }
 }
