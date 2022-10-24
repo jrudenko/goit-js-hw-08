76e95cf728edb8b3065a79f8f80cf7059d0f1ad5
Julia Rudenko <yrudenko@i.ua>
2022-10-24 14:44:07 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,33 @@
-import '../css/common.css';
-import '../css/03-feedback.css';
-
 import throttle from 'lodash.throttle';
-
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
-player.setCurrentTime(seconds);
+const savedTim = localStorage.getItem('videoplayer-current-time');
+if (savedTim) {
+  console.log(savedTim);
+}
 
-onPlay = throttle(data => {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000);
+player
+  .setCurrentTime(savedTim)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,44 +5,35 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const getData = localStorage.getItem(STORAGE_KEY);
-
-
-let formData = JSON.parse(getData) || {};
+let formData = {};
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-    input: document.querySelector('.feedback-form input'),
-};   
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
 
 refs.form.addEventListener('submit', onFormSummit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
-function onFormInput (e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));    
-};
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 
 populateTextarea();
 
-function onFormSummit(e) { 
-    e.preventDefault();
-    console.log('Submitting a form');
-    e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-};
+function onFormSummit(e) {
+  e.preventDefault();
+  console.log('Отправка');
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
 
 function populateTextarea() {
-    const savedMassage = formData;
-
-    if (savedMassage) {        
-        refs.input.value = savedMassage.input;
-        refs.textarea.value = savedMassage.textarea;       
-    }
-};
-
-
-
-
-
-
+  const savedMassage = JSON.parse(getData);
+  if (savedMassage) {
+    refs.input.value = savedMassage.input;
+    refs.textarea.value = savedMassage.textarea;
+  }
+}
